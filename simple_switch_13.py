b2bd4dc5683c3a8c73c4417f43cc9c25bc1a2be4
Yukinarii <h30286@gmail.com>
2018-04-12 01:42:10 +0800

    Update simple_switch_13.py

--- simple_switch_13.py
+++ simple_switch_13.py
@@ -13,10 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from operator import attregetter
+from operator import attrgetter
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
@@ -39,19 +39,18 @@ class SimpleSwitch13(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_change_handler(self, ev):
     	datapath = ev.datapath
-    	if ev.state == MAIN_DISPATCHER
+    	if ev.state == MAIN_DISPATCHER:
     	    if not datapath.id in self.datapaths:
     	    	self.logger.debug('register datapath: %016x', datapath.id)
     	    	self.datapaths[datapath.id] = datapath
-    	   elif ev.state is DEAD_DISPATCHER:
-    	   		if datapath.id in self.datapaths:
-    	   			self.logger.debug('unregister datapath: %016x', datapath.id)
-    	    		self.datapaths[datapath.id] = datapath
-    
+    	elif ev.state is DEAD_DISPATCHER:
+    	    if datapath.id in self.datapaths:
+    	   		self.logger.debug('unregister datapath: %016x', datapath.id)
+    			del self.datapaths[datapath.id]
     def _monitor(self):
     	while True:
     		for value in self.datapaths.values():
-    			self.request_states(value)
+    			self._request_state(value)
     		hub.sleep(10)
 
     def _request_state(self, datapath):
@@ -66,20 +65,36 @@ class SimpleSwitch13(app_manager.RyuApp):
     def _flow_stats_reply_handler(self, ev):
     	body = ev.msg.body
 
-    	self.logger.info('datapath        '
-    		             'in-port    eth-dst            '
-    		             'out-port   packets    bytes')
-    	self.logger.info('---------------------'
-    					 '-------------------------------'
-    					 '-------------------------------')
-    	for stat in sorted([flow for flow in body if flow.priority == 1],
-    		               key=lambda flow: (flow.match['in_port'],
-    		               	                  flow.match['eth_dst'])):
-    		self.logger.info('%016x %8x %17s %8x %8d %8d',
-    			             ev.msg.datapath.id,
-    			             stat.match['in_port'], stat.match['eth_dst'],
-    			             stat.instructions[0].actions[0].port,
-    			             stat.packet_count, stat.byte_count)
+        self.logger.info('<<=============leaved host1==========>>')
+        self.logger.info('datapath        '
+                         'in-port    eth-src            '
+                         'out-port   packets    bytes')
+        self.logger.info('---------------------'
+                         '-------------------------------'
+                         '-------------------------------')
+        for stat in body:
+            if stat.priority ==1 and stat.match['eth_src'] == "00:00:00:00:00:01":
+                self.logger.info('%016x %8x %17s %8x %8d %8d',
+                             ev.msg.datapath.id,
+                             stat.match['in_port'], stat.match['eth_src'],
+                             stat.instructions[0].actions[0].port,
+                             stat.packet_count, stat.byte_count)
+
+        self.logger.info('<<=============arrived host1==========>>')
+        self.logger.info('datapath        '
+                         'in-port    eth-dst            '
+                         'out-port   packets    bytes')
+        self.logger.info('---------------------'
+                         '-------------------------------'
+                         '-------------------------------')
+        for stat in sorted([flow for flow in body if flow.priority == 1],
+                           key=lambda flow: (flow.match['in_port'],
+                                              flow.match['eth_dst'])):
+            self.logger.info('%016x %8x %17s %8x %8d %8d',
+                             ev.msg.datapath.id,
+                             stat.match['in_port'], stat.match['eth_dst'],
+                             stat.instructions[0].actions[0].port,
+                             stat.packet_count, stat.byte_count)
 
 
 
